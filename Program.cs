ef248cb3d949ea0975f3769b6f169b5d98272fc6
Fredrik Wallander <f.m.wallander@gmail.com>
2021-04-28 13:31:56 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gp20_2021_0426_rest_gameserver_Exoduz85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
