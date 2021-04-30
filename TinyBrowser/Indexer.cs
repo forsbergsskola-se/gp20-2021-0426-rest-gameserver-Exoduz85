9cea8d1d3429aa7309d36f8b576c323cbb756f4a
Fredrik Wallander <f.m.wallander@gmail.com>
2021-04-30 14:06:05 +0200

    small cleanup

--- TinyBrowser/Indexer.cs
+++ TinyBrowser/Indexer.cs
@@ -4,7 +4,6 @@ using System.Linq;
 
 namespace Extender {
     public static class Indexer {
-        // Find all the indexes of the string value, if no more index == -1 return list of indexes
         public static List<int> AllIndexesOf(this string str, string value) {
             if (String.IsNullOrEmpty(value))
                 throw new ArgumentException("the string to find may not be empty", "value");
@@ -16,7 +15,6 @@ namespace Extender {
                 indexes.Add(index);
             }
         }
-        // Find all the strings of titles in the string of returned characters from server and return them in a list
         public static List<string> GetTitle(this List<int> titleTags, int stepForward, List<int> endTitleTags, string title) {
             if (titleTags == null)
                 throw new ArgumentException("the list can not be empty:", $"TitleTags.Count = {titleTags.Count}");
--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -7,30 +7,23 @@ namespace gp20_2021_0426_rest_gameserver_Exoduz85 {
         static void Main(string[] args)
         {
             var title = "<title>acme.com</title>\n<title>github.com</title>\n<title>marc.com</title>\n<title>ReMarcAble.com</title>";
-            
             List<int> titleTags = title.AllIndexesOf(Constants.titleTag);
-            
             List<int> endTitleTags = title.AllIndexesOf(Constants.endTitleTag); 
-            
             List<string> titles = titleTags.GetTitle(Constants.stepForward, endTitleTags, title); 
-            
             foreach (var s in titles) {
                 Console.WriteLine(s);
             }
         }
     }
     public static class Constants {
-        // Start title tag
         public static string titleTag = "<title>"; 
-        // End title tag
         public static string endTitleTag = "</title>"; 
-        // Number of steps to pass "<title>" characters
         public static int stepForward = 7; 
     }
 }
-
-/*int first = title.IndexOf("<title>") + 7; // first occurence
-int last = title.LastIndexOf("</title>"); // last occurence
+/*
+var string = "";
+int first = title.IndexOf('s') + 1; // first
+int last = title.LastIndexOf('d'); // last
 var text = title[first..last]; // get range
-Console.WriteLine($"first: {first}, last: {last}");
-Console.WriteLine(text);*/
+*/
