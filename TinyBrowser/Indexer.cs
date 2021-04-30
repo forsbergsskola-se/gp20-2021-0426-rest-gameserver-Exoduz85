e78e91363536997e382c2356c7467a3a3cadc49a
Fredrik Wallander <f.m.wallander@gmail.com>
2021-04-30 11:13:48 +0200

    adds example code of getting all titles

---
+++ TinyBrowser/Indexer.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extender {
+    public static class Indexer {
+        // Find all the indexes of the string value, if no more index == -1 return list of indexes
+        public static List<int> AllIndexesOf(string str, string value) {
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", "value");
+            List<int> indexes = new List<int>();
+            for (int index = 0; ; index += value.Length) {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    return indexes;
+                indexes.Add(index);
+            }
+        }
+        // Find all the strings of titles in the string of returned characters from server and return them in a list
+        public static List<string> GetTitle(List<int> titleTags, int stepForward, List<int> endTitleTags, string title) {
+            if (titleTags == null)
+                throw new ArgumentException("the list can not be empty:", $"TitleTags.Count = {titleTags.Count}");
+            return (from position in titleTags let first = position + stepForward let last = endTitleTags[titleTags.IndexOf(position)] select title[first..last]).ToList();
+        }
+    }
+}
--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using Extender;
 
-namespace gp20_2021_0426_rest_gameserver_Exoduz85
-{
-    class Program
-    {
+namespace gp20_2021_0426_rest_gameserver_Exoduz85 {
+    class Program { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // String of titles returned from site
+            var title = "<title>acme.com</title>\n<title>github.com</title>\n<title>marc.com</title>\n<title>ReMarcAble.com</title>";
+            // Get start index of each title
+            List<int> titleTags = Indexer.AllIndexesOf(title, Constants.titleTag); 
+            // Get end index of each title
+            List<int> endTitleTags = Indexer.AllIndexesOf(title, Constants.endTitleTag); 
+            // Get all titles from indexes
+            List<string> titles = Indexer.GetTitle(titleTags, Constants.stepForward, endTitleTags, title); 
+            // Print out all indexes that where found.
+            foreach (var s in titles) {
+                Console.WriteLine(s);
+            }
         }
     }
+    public static class Constants {
+        // Start title tag
+        public static string titleTag = "<title>"; 
+        // End title tag
+        public static string endTitleTag = "</title>"; 
+        // Number of steps to pass "<title>" characters
+        public static int stepForward = 7; 
+    }
 }
+
+/*int first = title.IndexOf("<title>") + 7; // first occurence
+int last = title.LastIndexOf("</title>"); // last occurence
+var text = title[first..last]; // get range
+Console.WriteLine($"first: {first}, last: {last}");
+Console.WriteLine(text);*/
