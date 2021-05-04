59a3fd4b9cadd0095cc204546a01837743850789
Fredrik Wallander <f.m.wallander@gmail.com>
2021-05-04 11:37:26 +0200

    test

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -24,32 +24,17 @@ namespace gp20_2021_0426_rest_gameserver_Exoduz85 {
             var encoding = Encoding.ASCII.GetBytes(request);
             stream.Write(encoding, 0, encoding.Length);
             var response = sr.ReadToEnd();
-            Console.WriteLine(response);
-            /*List<int> titleTags = response.AllIndexesOf(Constants.titleTag);
-            List<int> endTitleTags = response.AllIndexesOf(Constants.endTitleTag); 
-            List<string> titles = titleTags.GetString(Constants.stepForward, endTitleTags, response); 
-            foreach (var s in titles) {
-                Console.WriteLine(s);
-            }*/
-            List<int> htmlTags = response.AllIndexesOf(Constants.htmlTag);
-            List<int> endHtmlTags = response.AllIndexesOf(Constants.endHtmlTag);
-            List<string> htmls = htmlTags.GetString(9, endHtmlTags, response);
-            foreach (var html in htmls) {
-                Console.WriteLine(html);
-            }
+            //Console.WriteLine(response);
+            int first = response.IndexOf(Constants.titleTag) + Constants.stepForward;
+            int last = response.LastIndexOf(Constants.endTitleTag);
+            var text = response[first..last];
+            Console.WriteLine(text);
+            
         }
     }
     public static class Constants {
         public static string titleTag = "<title>"; 
-        public static string endTitleTag = "</title>"; 
-        public static string htmlTag = "<a href=\""; 
-        public static string endHtmlTag = "\">";
+        public static string endTitleTag = "</title>";
         public static int stepForward = 7; 
     }
 }
-/*
-var string = "";
-int first = title.IndexOf('s') + 1; // first
-int last = title.LastIndexOf('d'); // last
-var text = title[first..last]; // get range
-*/
