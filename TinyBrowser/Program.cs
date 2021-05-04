e72ed24936789cd8b9f89ee50b4cfc6310e87585
Fredrik Wallander <f.m.wallander@gmail.com>
2021-05-04 10:30:04 +0200

    gets the first stream from acme

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,18 +1,41 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
 using Extender;
 
 namespace gp20_2021_0426_rest_gameserver_Exoduz85 {
-    class Program { 
-        static void Main(string[] args)
-        {
-            var title = "<title>acme.com</title>\n<title>github.com</title>\n<title>marc.com</title>\n<title>ReMarcAble.com</title>";
+    class Program {
+        const int port = 80;
+        const string hostUrl = "acme.com";
+        static readonly string request = $"GET / HTTP/1.1\r\nHost: {hostUrl}\r\n\r\n";
+        static StreamReader sr;
+        static StreamWriter sw;
+        static NetworkStream stream;
+        static TcpClient client;
+        static void Main(string[] args) {
+            
+            client = new TcpClient(hostUrl, port);
+            stream = client.GetStream();
+            sw = new StreamWriter(stream);
+            sr = new StreamReader(stream);
+            var encoding = Encoding.ASCII.GetBytes(request);
+            stream.Write(encoding, 0, encoding.Length);
+            //sw.Write(request, 0, request.Length);
+            var response = sr.ReadToEnd();
+            Console.WriteLine(response);
+
+
+            /*var title = "<title>acme.com</title>\n<title>github.com</title>\n<title>marc.com</title>\n<title>ReMarcAble.com</title>";
             List<int> titleTags = title.AllIndexesOf(Constants.titleTag);
             List<int> endTitleTags = title.AllIndexesOf(Constants.endTitleTag); 
             List<string> titles = titleTags.GetTitle(Constants.stepForward, endTitleTags, title); 
             foreach (var s in titles) {
                 Console.WriteLine(s);
-            }
+            }*/
         }
     }
     public static class Constants {
