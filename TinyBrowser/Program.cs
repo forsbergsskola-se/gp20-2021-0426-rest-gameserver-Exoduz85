e3764e5555b14da85449ccd52809e46440e0b155
Fredrik Wallander <f.m.wallander@gmail.com>
2021-05-05 09:18:09 +0200

    redo

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
-using Extender;
 
 namespace gp20_2021_0426_rest_gameserver_Exoduz85 {
     class Program {
@@ -23,25 +21,16 @@ namespace gp20_2021_0426_rest_gameserver_Exoduz85 {
             var encoding = Encoding.ASCII.GetBytes(request);
             stream.Write(encoding, 0, encoding.Length);
             var response = sr.ReadToEnd();
-            //Console.WriteLine(response);
-            int first = response.IndexOf(Constants.titleTag, StringComparison.OrdinalIgnoreCase) + Constants.stepForward;
-            int last = response.LastIndexOf(Constants.endTitleTag, StringComparison.OrdinalIgnoreCase);
-            var text = response[first..last];
-            Console.WriteLine(text);
+            var header = ExtractHeader(response);
             
-            List<int> htmlTag = response.AllIndexesOf("<a");
-            List<int> endHtmlTag = response.AllIndexesOf("</a>"); 
-            List<string> titles = htmlTag.GetString(9, endHtmlTag , response); 
-            foreach (var s in titles) {
-                Console.WriteLine(s);
-            }
+            Console.WriteLine(header);
             
             client.Close();
         }
-    }
-    public static class Constants {
-        public static string titleTag = "<title>"; 
-        public static string endTitleTag = "</title>";
-        public static int stepForward = 7; 
+        public static string ExtractHeader(string str) {
+            int first = str.IndexOf("<title>", StringComparison.OrdinalIgnoreCase) + 7;
+            int last = str.LastIndexOf("</title>", StringComparison.OrdinalIgnoreCase);
+            return str[first..last];
+        }
     }
 }
