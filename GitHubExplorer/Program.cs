557d427b91014ee88acda031ffda17e250e32d05
Fredrik Wallander <f.m.wallander@gmail.com>
2021-05-14 20:40:36 +0200

    adds repository getter with users repos

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,46 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace GitHubExplorer
 {
-    public class User {
-        public string login{get;set;}
-        public int id {get; set;}
-        public string node_id {get; set;}
-        public string avatar_url {get; set;}
-        public string gravatar_id {get; set;}
-        public string url {get; set;}
-        public string html_url {get; set;}
-        public string followers_url {get; set;}
-        public string following_url {get; set;}
-        public string gists_url {get; set;}
-        public string starred_url {get; set;}
-        public string subscriptions_url {get; set;}
-        public string organizations_url {get; set;}
-        public string repos_url {get; set;} 
-        public string events_url {get; set;}
-        public string received_events_url {get; set;}
-        public string type {get; set;}
-        public bool site_admin {get; set;}
-        public string name {get; set;}
-        public string company {get; set;}
-        public string blog {get; set;}
-        public string location {get; set;}
-        public string email {get; set;}
-        public string bio {get; set;}
-        public string twitter_username {get; set;}
-        public int public_repos {get; set;}
-        public int public_gists {get; set;}
-        public int followers {get; set;}
-        public int following {get; set;}
-        public string created_at {get; set;}
-        public string updated_at {get; set;}
-    }
     static class Program {
         static string userName;
-        const string baseUri = "https://api.github.com/users/";
         static void Main(string[] args) {
             while (true) {
                 var client = new HttpClient
@@ -50,24 +17,33 @@ namespace GitHubExplorer
                         Accept = {new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")},
                         UserAgent = {ProductInfoHeaderValue.Parse("request")},
                         Authorization = new AuthenticationHeaderValue("token", Secret.token)
-                    }
+                    },
+                    BaseAddress = new Uri("https://api.github.com/users/")
                 };
                 Console.WriteLine("Hello, what user would you like to browse?");
                 userName = Console.ReadLine();
-                client.BaseAddress = new Uri(baseUri + userName);
-                var response = client.GetStringAsync(client.BaseAddress);
-                User user = JsonSerializer.Deserialize<User>(response.Result);
-                Console.WriteLine($"{user.name}, {user.location}.");
                 
-                Console.WriteLine("What would you like to see next? \n(0) Followers\n(1) Organizations\n(2) Repositories\n(3) Quit");
+                var response = client.GetStringAsync(client.BaseAddress + userName);
+                User user = JsonSerializer.Deserialize<User>(response.Result);
+                Console.WriteLine($"{user.name}, {user.location}.\n");
+                
+                Console.WriteLine("What would you like to see next? \n(0) Followers\n(1) Organizations\n(2) Repositories\n(3) Quit\n");
                 var userSelect = int.TryParse(Console.ReadLine(), out var selection);
+                
                 if (userSelect) {
                     switch (selection) {
                         case 0:
+                            Console.WriteLine($"Number of followers: {user.followers}.");
                             break;
                         case 1:
+                            Console.WriteLine($"{user.name} is in organizations: {user.organizations_url}.");
                             break;
                         case 2:
+                            var repos = client.GetStringAsync(client.BaseAddress + userName + "/repos");
+                            List<Repository> repositories = JsonSerializer.Deserialize<List<Repository>>(repos.Result);
+                            foreach (var repo in repositories) {
+                                Console.WriteLine($"Repository name: {repo.name}, id: {repo.id}");
+                            }
                             break;
                         case 3:
                             return;
@@ -76,7 +52,7 @@ namespace GitHubExplorer
                 else {
                     client.Dispose();
                     Console.WriteLine("Not a valid input!");
-                    break;
+                    return;
                 }
             }
         }
---
+++ GitHubExplorer/Repository.cs
@@ -0,0 +1,7 @@
+namespace GitHubExplorer {
+    public class Repository {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string full_name { get; set; }
+    }
+}
