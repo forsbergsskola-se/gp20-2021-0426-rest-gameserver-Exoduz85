7f76db91d03a0b2c05b2a29a2747b9debd2ec0d7
Fredrik Wallander <f.m.wallander@gmail.com>
2021-05-18 22:44:04 +0200

    Adds async task to return number of available scooters

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace LameScooter
@@ -7,14 +10,28 @@ namespace LameScooter
     class Program
     {
         static async Task Main(string[] args) {
-            ILameScooterRental rental = null;
+            ILameScooterRental rental = new OfflineLameScooterRental();
             var count = await rental.GetScooterCountInStation(args[0]);
             Console.WriteLine($"Number of Scooters Available at {args[0]}: {count}");
         }
     }
+    public class OfflineLameScooterRental : ILameScooterRental {
+        async Task<Stations> LoadStations() {
+            using var sr = new StreamReader(Path.Combine(Environment.CurrentDirectory, "scooters.json"));
+            return JsonSerializer.Deserialize<Stations>(await sr.ReadToEndAsync());
+        }
+        public Task<int> GetScooterCountInStation(string stationName) {
+            var station = LoadStations().Result.stations.Where(a => a.name == stationName).ToArray();
+            if(!station.Any()) throw new InvalidDataException($"{stationName} is not Valid");
+            return Task.FromResult(station.Select(a => a.bikesAvailable).FirstOrDefault());
+        }
+    }
 
+    public class Stations {
+        public List<Station> stations { get; set; }
+    }
     public class Station{
-        public int id { get; set; }
+        public string id { get; set; }
         public string name { get; set; }
         public float x { get; set; }
         public float y { get; set; }
@@ -26,7 +43,7 @@ namespace LameScooter
         public bool isFloatingBike { get; set; }
         public bool isCarStation { get; set; }
         public string state { get; set; }
-        public List<string> networks { get; set; }
+        public string[] networks { get; set; }
         public bool realTimeData { get; set; }
     }
     public interface ILameScooterRental
