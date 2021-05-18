86a9c27064b17d9612883582b7d72561a5188422
Fredrik Wallander <f.m.wallander@gmail.com>
2021-05-18 14:20:42 +0200

    adds station class and prints args to console (plus json station file)

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LameScooter
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(args);
         }
+    }
+
+    public class Station {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float x { get; set; }
+        public float y { get; set; }
+        public int bikesAvailable { get; set; }
+        public int spaceAvailable { get; set; }
+        public int capacity { get; set; }
+        public bool allowDropOff { get; set; }
+        public bool allowOverloading { get; set; }
+        public bool isFloatingBike { get; set; }
+        public bool isCarStation { get; set; }
+        public string state { get; set; }
+        public List<string> networks { get; set; }
+        public bool realTimeData { get; set; }
     }
 }
