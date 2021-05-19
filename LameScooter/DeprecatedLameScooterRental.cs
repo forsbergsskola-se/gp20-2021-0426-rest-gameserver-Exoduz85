2089baedf2155b8ebb0ba209c896be83ebbb6d59
Fredrik Wallander <f.m.wallander@gmail.com>
2021-05-19 16:02:21 +0200

    Adds deprecatedLameScooterRental working

--- LameScooter/DeprecatedLameScooterRental.cs
+++ LameScooter/DeprecatedLameScooterRental.cs
@@ -1,10 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LameScooter {
     public class DeprecatedLameScooterRental : ILameScooterRental {
+        async Task<List<Station>> LoadStations() {
+            using var sr = new StreamReader(Path.Combine(Environment.CurrentDirectory, "scooters.txt"));
+            var result= DeprecatedDeserialized(await sr.ReadToEndAsync());
+            return result;
+        }
         public Task<int> GetScooterCountInStation(string stationName) {
-            throw new NotImplementedException();
+            var station = LoadStations().Result.Where(a => a.name == stationName).ToArray();
+            if (station.Any()) return Task.FromResult(station.Select(a => a.bikesAvailable).FirstOrDefault());
+            var NotFoundException = new Exception($"Could not find: {stationName}");
+            throw NotFoundException;
+        }
+        static List<Station> DeprecatedDeserialized(string data) {
+            List<Station> result = new List<Station>();
+            var regex = new Regex("(?<name>\\w*?)\\s*?:\\s*?(?<count>\\d*?)\\r\\n",RegexOptions.IgnoreCase);
+            if(regex.IsMatch(data))
+                foreach(Match match in regex.Matches(data)) {
+                    Station station = new Station{
+                        name = match.Groups["name"].Value,
+                        bikesAvailable = int.Parse(match.Groups["count"].Value)
+                    };
+                    result.Add(station);
+                }
+            return result;
         }
     }
 }
--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -7,10 +7,17 @@ namespace LameScooter
     class Program
     {
         static async Task Main(string[] args) {
-            ILameScooterRental rental = new OfflineLameScooterRental();
             try {
                 if (args[0].Any(char.IsDigit)) throw new ArgumentException("Argument may not contain any numbers");
-                
+                var validArgs = new [] {"offline", "deprecated", "realtime","mongodb"};
+                if(!validArgs.Contains(args[1].ToLower()))throw new ArgumentException("Not a valid argument");
+                ILameScooterRental rental = args[1].ToLower() switch {
+                    "offline" => new OfflineLameScooterRental(),
+                    "deprecated" => new DeprecatedLameScooterRental(),
+                    //"realtime" => new RealTimeLameScooterRental(),
+                    //"mongodb" => new MongoDBLameScooterRental(),
+                    _ => null
+                };
                 var count = await rental.GetScooterCountInStation(args[0]);
                 Console.WriteLine($"Number of Scooters Available at {args[0]}: {count}");
             }
